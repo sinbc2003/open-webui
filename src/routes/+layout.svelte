8ad99e75a880dd298f13ef43c2338670c301d2f7
sinbc2003 <128018744+sinbc2003@users.noreply.github.com>
2025-02-18 01:36:19 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -474,7 +474,7 @@
 		if (backendConfig) {
 			// Save Backend Status to Store
 			await config.set(backendConfig);
-			await WEBUI_NAME.set(backendConfig.name);
+			await WEBUI_NAME.set(Sawl GPT);
 
 			if ($config) {
 				await setupSocket($config.features?.enable_websocket ?? true);
